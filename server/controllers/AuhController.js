fc3951536f2b322d76ba525d062631da3dff4297
chandu <hemachandubenuboyina@gmail.com>
2025-10-08 16:25:57 +0530

    server side implementation

--- server/controllers/AuhController.js
+++ server/controllers/AuhController.js
@@ -7,6 +7,8 @@ import { fileURLToPath } from "url";
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
+
+
 const createToken = (email, userId) => {
   return jwt.sign({ email, userId }, process.env.JWT_SECRET, {
     expiresIn: "3d",
